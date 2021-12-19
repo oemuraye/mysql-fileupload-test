cbd1e647524330252d9cf426987be39231f07b8a
Emuraye Pius 0 <76441493+oemuraye@users.noreply.github.com>
2021-12-19 23:21:54 +0100

    added extra features

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const fileUpload = require("express-fileupload");
 const mysql = require("mysql");
 
 const dotenv = require("dotenv");
+const Pool = require("mysql/lib/Pool");
 dotenv.config();
 
 const app = express();
@@ -12,33 +13,43 @@ const port = process.env.PORT || 5000;
 app.use(fileUpload());
 
 // Static Files
-app.use(express.static('public'));
-app.use(express.static('upload'));
+app.use(express.static("public"));
+app.use(express.static("upload"));
 
 const handlebars = exphbs.create({ extname: ".hbs" });
 app.engine(".hbs", handlebars.engine);
 app.set("view engine", ".hbs");
 
 // Connection Pool
-var connection = mysql.createConnection({
+const pool = mysql.createPool({
+  connectionLimit: 10,
   host: process.env.HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
-  database: process.env.DATABASE
+  database: process.env.DATABASE,
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.log(err.message);
-  }
+pool.getConnection((err, connection) => {
+  if (err) throw err;
   console.log("db " + connection.state);
 });
 
-app.get('/', (req, res) => {
-    res.render('index')
-})
+app.get("", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err; // not connected
+    console.log("Connected!");
 
-app.post('/', (req, res) => {
+    connection.query('SELECT * FROM user WHERE id = "1"', (err, rows) => {
+      // Once done, release connection
+      connection.release();
+      if (!err) {
+        res.render("index", { rows });
+      }
+    });
+  });
+});
+
+app.post("/", (req, res) => {
   let sampleFile;
   let uploadPath;
 
@@ -54,10 +65,26 @@ app.post('/', (req, res) => {
   sampleFile.mv(uploadPath, (err) => {
     if (err) return res.status(500).send(err);
 
-    res.send("uploaded");
+    
+    app.get("", (req, res) => {
+        pool.getConnection((err, connection) => {
+            if (err) throw err; // not connected
+            console.log("Connected!");
+            
+            connection.query('UPDATE * FROM user SET profile_image = ? WHERE id = "1"', [sampleFile.name], (err, rows) => {
+         
+         
+            // Once done, release connection
+         if (!err) {
+           res.redirect("/");
+         } else {
+           console.log(err);
+         }
+        })
+    });
+    // res.send("uploaded");
+    });
   });
-})
-
-
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
